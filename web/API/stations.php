2a4e21fde337a5d8e1e80cbe19542030591d7526
Lukáš Plevač <lukas@plevac.eu>
2023-10-01 11:50:55 +0200

    Added support dunction for get all api keys

--- web/API/stations.php
+++ web/API/stations.php
@@ -3,9 +3,20 @@
 
     function APIinfo($params) {
 
-        $station  = new \DAL\station()
+        $station  = new \DAL\station();
 
-        $station->find("apiKey", $params["key"])
+        $station->find("apiKey", $params["key"]);
 
         return ["id" => $station->id->get(), "name" => $station->name->get(), "locator" => $station->locator->get()];
+    }
+
+    function keys($params) {
+        $stations  = new \wsos\database\core\table(\DAL\station::class);
+
+        $res = [];
+        foreach ($stations->getAll()->values as $station) {
+            $res[] = ["name" => $station->name->get(), "key" => $station->apiKey->get()];
+        }
+
+        return $res;
     }
